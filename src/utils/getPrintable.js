5f5fe73456202ace39e04661eefa4142da827a13
Der Meyer <simon.der.meyer@gmail.com>
2022-01-29 17:51:04 +0100

    filter length 0 gymnastics result from printable. Keep build dir on server when CI runs

--- src/utils/getPrintable.js
+++ src/utils/getPrintable.js
@@ -1,54 +1,69 @@
 import { getRanks } from './getRanks';
-import { AgesPerGroup, MeasureUnit } from '../competition-logic/values';
-import { isObject } from './helpers';
+import { AgesPerGroup, Exceptional, MeasureUnit } from '../competition-logic/values';
 import { getPoints } from '../competition-logic/points';
 
-const fallbackString = '-';
+const fallbackPrintValue = '-';
+const maxResultsForEventLength = 5;
+
 
 export function getPrintable({ competitors, activeEvent, activeEventIds }) {
-    const ranked = getRanks(competitors, activeEventIds);
+    return getRanks(competitors, activeEventIds)
+        .map(competitor => {
+            const { name, gender, club, points, rank, year } = competitor;
 
-    return ranked.map(competitor => {
-        const { name, gender, club, points, rank, year } = competitor;
+            const { name: eventName, date: eventDate, final: eventIsFinal } = activeEvent;
 
-        const { name: eventName, date: eventDate, final: eventIsFinal } = activeEvent;
+            const weightAtEvent = competitor.weight[activeEvent.id] || fallbackPrintValue;
 
-        const weightAtEvent = competitor.weight[activeEvent.id] || fallbackString;
+            const ageAtEvent = new Date(activeEvent.date).getFullYear() - competitor.year;
+            const ageGroupAtEvent = Object.keys(AgesPerGroup).find(group => AgesPerGroup[group].includes(ageAtEvent));
 
-        const ageAtEvent = new Date(activeEvent.date).getFullYear() - competitor.year;
-        const ageGroupAtEvent = Object.keys(AgesPerGroup).find(group => AgesPerGroup[group].includes(ageAtEvent));
+            const rawResults = { ...competitor.results[activeEvent.id] } || {};
+            const resultsForEvent = Object.keys(rawResults)
+                .reduce((acc, discipline) => {
+                    const result = rawResults[discipline];
+                    switch (discipline) {
+                        case Exceptional.GYMNASTICS: {
+                            const numGymnastics = Object.keys(result).length;
+                            if (numGymnastics === 0) {
+                                return acc;
+                            }
+                            return [
+                                ...acc,
+                                {
+                                    discipline,
+                                    result: `${numGymnastics} ${MeasureUnit[discipline]}`,
+                                    points: `${Object.values(result).reduce((r, e) => r + parseInt(e), 0)} Punkte`
+                                }
+                            ];
+                        }
+                        default:
+                            return [
+                                ...acc,
+                                {
+                                    discipline,
+                                    result: typeof result !== 'undefined' ? `${result} ${MeasureUnit[discipline]}` : fallbackPrintValue,
+                                    points: typeof result !== 'undefined' ? `${getPoints(discipline, result)} Punkte` : fallbackPrintValue
+                                }
+                            ];
+                    }
+                }, [])
+                .slice(0, maxResultsForEventLength);
 
-        const rawResults = { ...competitor.results[activeEvent.id] } || {};
-        const resultsForEvent = Object.keys(rawResults).map(discipline => {
-            const result = rawResults[discipline];
-            if (!isObject(result)) {
-                return {
-                    discipline,
-                    result: typeof result !== 'undefined' ? `${result} ${MeasureUnit[discipline]}` : fallbackString,
-                    points: typeof result !== 'undefined' ? `${getPoints(discipline, result)} Punkte` : fallbackString
-                };
-            }
-            // gymnastics results
             return {
-                discipline,
-                result: `${Object.keys(result).length} ${MeasureUnit[discipline]}`,
-                points: `${Object.values(result).reduce((r, e) => r + parseInt(e), 0)} Punkte`
-            }
-        }).slice(0, 5);
-
-        return {
-            name,
-            gender,
-            club,
-            points,
-            rank,
-            year,
-            eventName,
-            eventDate,
-            eventIsFinal,
-            weightAtEvent,
-            ageGroupAtEvent,
-            resultsForEvent
-        };
-    }).filter(e => !!e.name && !!e.gender && !!e.club && !!e.year);
+                name,
+                gender,
+                club,
+                points,
+                rank,
+                year,
+                eventName,
+                eventDate,
+                eventIsFinal,
+                weightAtEvent,
+                ageGroupAtEvent,
+                resultsForEvent
+            };
+        })
+        .filter(e => !!e.name && !!e.gender && !!e.club && !!e.year);
 }
